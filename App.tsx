e9f17633058f5c6ba5c1569bf0a6ee4f2adad9af
baptistegoncalves <146066736+baptistegoncalves@users.noreply.github.com>
2024-09-25 09:59:34 +0200

    upload de base

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-500">
+      <Text className='text-2xl'>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
