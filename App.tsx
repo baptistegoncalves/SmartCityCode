0af5774d71b0b8be38e59af6f0140f8469375364
baptistegoncalves <baptiste.goncalves@epitech.digital>
2024-09-25 13:33:25 +0200

    Ajout de tous les datasets, suppression de tailwind et nativewind

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-green-500">
-      <Text className='text-2xl'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar/>
     </View>
   );
 }
