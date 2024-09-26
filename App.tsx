45e2497367eaed157009ecc66940ce7cf8d84499
Antonin planel <antonin@Antonins-MacBook-Air.local>
2024-09-26 10:29:58 +0200

    fin map

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./components/HomeScreen_vanto";
+import MapScreen from "./components/Mapscreen"; // Assurez-vous que le nom du fichier est correct
+
+type RootStackParamList = {
+  Home: undefined;
+  Map: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-green-500">
-      <Text className='text-2xl'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
