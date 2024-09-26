9b7827c06d8a56134f6c053a9d5d6c26e70a3345
Antonin planel <antonin.planel09@icloud.com>
2024-09-26 13:10:31 +0200

    fusion map login

--- App.tsx
+++ App.tsx
@@ -1,32 +1,28 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./components/HomeScreen_vanto";
 import MapScreen from "./components/Mapscreen"; // Assurez-vous que le nom du fichier est correct
-import { SafeAreaView, StatusBar } from 'react-native';
-import Login from './components/Login';
+import Login from "./components/Login";
+
 type RootStackParamList = {
-  Home: undefined;
+  Login: undefined;
   Map: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-
-
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-      <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
-      <Login />
-    </SafeAreaView>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Map" component={MapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-const Login = () => {
+type RootStackParamList = {
+  Login: undefined;
+  Map: undefined;
+};
+
+type LoginProps = NativeStackScreenProps<RootStackParamList, "Login">;
+
+const Login = ({ navigation }: LoginProps) => {
   return (
     <View style={styles.container}>
       {/* Fond de la carte */}
-      <Image source={require('../assets/imageAriane/loginfond.jpg')} style={styles.mapImage} />
-
+      <Image
+        source={require("../assets/imageAriane/loginfond.jpg")}
+        style={styles.mapImage}
+      />
       {/* Rectangle blanc centré */}
       <View style={styles.whiteBox}>
         {/* Ligne contenant le titre et le logo à droite */}
         <View style={styles.logoTitleContainer}>
           <Text style={styles.title}>Your way</Text>
-          <Image source={require('../assets/imageAriane/logoyourway.png')} style={styles.logo} />
+          <Image
+            source={require("../assets/imageAriane/logoyourway.png")}
+            style={styles.logo}
+          />
         </View>
-
-        <TouchableOpacity style={styles.loginButton}>
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("Map")}
+        >
           <Text style={styles.loginText}>Se connecter</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Map")}>
           <Text style={styles.signUpText}>S'inscrire</Text>
         </TouchableOpacity>
       </View>
@@ -30,14 +44,14 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    position: 'relative',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    position: "relative",
   },
   logoTitleContainer: {
-    flexDirection: 'row', // Aligner le titre et le logo sur la même ligne
-    alignItems: 'center', // Aligner verticalement au centre
+    flexDirection: "row", // Aligner le titre et le logo sur la même ligne
+    alignItems: "center", // Aligner verticalement au centre
     marginBottom: 30,
   },
   logo: {
@@ -47,41 +61,41 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 35,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   loginButton: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     paddingVertical: 15,
     paddingHorizontal: 40,
     borderRadius: 5,
     marginBottom: 20,
   },
   loginText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   signUpText: {
-    color: '#007AFF',
+    color: "#007AFF",
     fontSize: 16,
     marginTop: 10,
   },
   mapImage: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
     right: 0,
     zIndex: -1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   whiteBox: {
-    backgroundColor: 'white',
-    width: '80%',
+    backgroundColor: "white",
+    width: "80%",
     padding: 20,
     borderRadius: 10,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.8,
     shadowRadius: 2,
