1a966fa2865e28591d6eca07e4be8ef2f3683d26
Antonin planel <antonin.planel09@icloud.com>
2024-09-26 13:45:49 +0200

    resolved

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import * as React from "react";
+import { useState } from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MapScreen from "./components/Mapscreen"; // Assurez-vous que le nom du fichier est correct
+import Login from "./components/Login";
+import MainPopup from './components/MainPopup';
+type RootStackParamList = {
+  Login: undefined;
+  Map: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const App = () => {
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const togglePopup = () => {
+    setPopupOpen(!isPopupOpen);
+  };
   return (
     <View className="flex-1 justify-center items-center bg-green-500">
       <Text className='text-2xl'>Open up App.tsx to start working on your app!</Text>
