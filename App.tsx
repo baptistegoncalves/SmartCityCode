0d5b4098eefefb80dcedb59bc7deb58fdfb5b41b
Antonin planel <antonin.planel09@icloud.com>
2024-09-27 13:38:32 +0200

    correction

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./components/HomeScreen_vanto";
+import * as React from "react";
+
 import MapScreen from "./components/Mapscreen"; // Assurez-vous que le nom du fichier est correct
 
 type RootStackParamList = {
@@ -15,7 +15,6 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
       </Stack.Navigator>
     </NavigationContainer>
