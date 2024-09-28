327e67375a91f0736bab20342b36f87203ff1774
Antonin planel <antonin.planel09@icloud.com>
2024-09-28 16:36:40 +0200

    option fini

--- components/Mapscreen.tsx
+++ components/Mapscreen.tsx
@@ -8,23 +8,24 @@ import {
   KeyboardAvoidingView,
   Platform,
   Text,
+  TouchableOpacity,
 } from "react-native";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import MainPopup from "./MainPopup"; // Importez MainPopup
+import OptionSelector from "./OptionSelector"; // Importez OptionSelector
 
-type RootStackParamList = {
-  Home: undefined;
-  Map: undefined;
-};
-
+type RootStackParamList = { Home: undefined; Map: undefined };
 type MapScreenProps = NativeStackScreenProps<RootStackParamList, "Map">;
 
 const { width, height } = Dimensions.get("window");
 const ASPECT_RATIO = width / height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const options = ["Rapide", "Sécurité", "Sain", "Calme"] as const;
+type Option = (typeof options)[number];
 
 function MapScreen({ navigation }: MapScreenProps) {
   const [region, setRegion] = React.useState({
@@ -33,11 +34,11 @@ function MapScreen({ navigation }: MapScreenProps) {
     latitudeDelta: LATITUDE_DELTA,
     longitudeDelta: LONGITUDE_DELTA,
   });
-
   const [location, setLocation] =
     React.useState<Location.LocationObject | null>(null);
-
   const [isPopupOpen, setIsPopupOpen] = React.useState(false);
+  const [selectedOptions, setSelectedOptions] = React.useState<Option[]>([]);
+  const [showOptions, setShowOptions] = React.useState(false);
 
   React.useEffect(() => {
     (async () => {
@@ -46,7 +47,6 @@ function MapScreen({ navigation }: MapScreenProps) {
         Alert.alert("Permission to access location was denied");
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
       setRegion({
@@ -55,7 +55,6 @@ function MapScreen({ navigation }: MapScreenProps) {
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       });
-
       Location.watchPositionAsync(
         {
           accuracy: Location.Accuracy.High,
@@ -84,6 +83,24 @@ function MapScreen({ navigation }: MapScreenProps) {
     setIsPopupOpen(!isPopupOpen);
   };
 
+  const handleSelectOption = (option: Option) => {
+    setSelectedOptions((prevSelectedOptions) => {
+      if (prevSelectedOptions.includes(option)) {
+        return prevSelectedOptions.filter((opt) => opt !== option);
+      } else {
+        return [...prevSelectedOptions, option];
+      }
+    });
+    // Logique pour ajuster l'itinéraire en fonction des options sélectionnées
+  };
+
+  const toggleOptions = () => {
+    setShowOptions(!showOptions);
+    if (isPopupOpen) {
+      setIsPopupOpen(false); // Fermer le pop-up si les options sont affichées
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -98,7 +115,18 @@ function MapScreen({ navigation }: MapScreenProps) {
       <View style={styles.buttonContainer}>
         <Button title="Recenter" onPress={centerMap} />
       </View>
-      <MainPopup togglePopup={togglePopup} />
+      <TouchableOpacity style={styles.toggleButton} onPress={toggleOptions}>
+        <Text style={styles.toggleButtonText}>
+          {showOptions ? "Hide Options" : "Show Options"}
+        </Text>
+      </TouchableOpacity>
+      {showOptions && (
+        <OptionSelector
+          selectedOptions={selectedOptions}
+          onSelectOption={handleSelectOption}
+        />
+      )}
+      {!showOptions && <MainPopup togglePopup={togglePopup} />}
     </KeyboardAvoidingView>
   );
 }
@@ -115,12 +143,24 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     position: "absolute",
-    bottom: 250, // Ajustez cette valeur pour positionner le bouton pas trop en bas
+    bottom: 250,
     left: 10,
     backgroundColor: "white",
     borderRadius: 5,
     padding: 10,
-    zIndex: 1, // Assurez-vous que le bouton est au-dessus de la carte
+    zIndex: 1,
+  },
+  toggleButton: {
+    position: "absolute",
+    top: 50,
+    left: 10,
+    backgroundColor: "white",
+    borderRadius: 5,
+    padding: 10,
+    zIndex: 1,
+  },
+  toggleButtonText: {
+    color: "black",
   },
 });
 
